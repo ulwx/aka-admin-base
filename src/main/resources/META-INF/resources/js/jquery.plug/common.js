47ba8ac6be3e6d11dcd016e2ba8ca833cd7026aa
maintainer <sunchaojin@163.com>
2024-06-11 19:13:24 +0800

    更新6月7日

--- src/main/resources/META-INF/resources/js/jquery.plug/common.js
+++ src/main/resources/META-INF/resources/js/jquery.plug/common.js
@@ -72,8 +72,16 @@ function isArray(obj){
 		 return true;
 	 }
 	 return false;
-} 
-
+}
+function formatDateTime(date) {
+	var year = date.getFullYear();
+	var month = ('0' + (date.getMonth() + 1)).slice(-2);
+	var day = ('0' + date.getDate()).slice(-2);
+	var hours = ('0' + date.getHours()).slice(-2);
+	var minutes = ('0' + date.getMinutes()).slice(-2);
+	var seconds = ('0' + date.getSeconds()).slice(-2);
+	return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+}
 function getDate(date){
 	var myDate
 	if(date){
