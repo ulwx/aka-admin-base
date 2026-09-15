5b900cee20beebe62ae83c6041a19ffa1bb9aa04
aka <39908619@qq.com>
2026-09-15 09:53:40 +0800

    更新

--- src/main/resources/META-INF/resources/js/ulwxbase/common.js
+++ src/main/resources/META-INF/resources/js/ulwxbase/common.js
@@ -931,6 +931,7 @@ function initDataGrid(selector, url, queryParams, columns, options) {
 		url : url,
 		striped : true,
 		columns : columns,
+		frozenColumns : (options.frozenColumns?options.frozenColumns:null),
 		pageSize:20,
 		border : false,
 		nowrap : false,
